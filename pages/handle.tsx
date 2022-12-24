b8e33bcf411a3dea3e903a9f55e23e5e95b86fbd
niyobern <niyobern@outlook.com>
2022-12-24 23:26:34 +0200

    tested

--- pages/handle.tsx
+++ pages/handle.tsx
@@ -1,32 +1,28 @@
-import { Form, useFormSubmit } from 'next-runtime/form';
 import { handle, json } from 'next-runtime';
+import { useFormSubmit, Form } from 'next-runtime/form';
 
 export const getServerSideProps = handle({
-    async post({ req: { body } }) {
-    //   await db.comments.insert({
-    //     message: body.message,
-    //   });
-  
-      return json({
-        message: 'thanks for your comment!',
-      });
-    },
-  })
+  async get({ params, query }) {
+    return json({ name: 'Stephan Meijer' });
+  },
 
-export default function MyPage({ name, message }: any) {
-  const { isSubmitting } = useFormSubmit();
+  async post({ req: { body } }) {
+    // console.log(body)
+    return json({ message: 'Thanks for your submission!', name: body.name });
+  },
+});
 
-  if (message) {
-    return <p>{message}</p>;
+export default function Home({ name, message }) {
+    
+  const form = useFormSubmit('bernardn');  
+  if (form.isSuccess) {
+    return <p>{`Thanks for your submission! ${form.values.name}`}</p>;
   }
 
   return (
-    <Form method="post">
+    <Form method="post" encType="multipart/form-data" name="bernardn">
       <input name="name" defaultValue={name} />
-      <input name="message" />
-      <button type="submit">
-        {isSubmitting ? 'submitting' : 'submit'}
-      </button>
+      <button type="submit">submit</button>
     </Form>
   );
 }
