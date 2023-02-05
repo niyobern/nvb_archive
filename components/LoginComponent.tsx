f466b8137ff5fb37dc7b8ac71149a80857fe8823
niyobern <niyobern@outlook.com>
2023-02-05 23:47:45 +0200

    done

--- components/LoginComponent.tsx
+++ components/LoginComponent.tsx
@@ -8,7 +8,7 @@ export default function LoginComponent(){
     <div className="flex lg:w-5/6 w-full  flex-col mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
         <div className="relative flex-grow w-full">
           <label htmlFor="number"className="leading-7 text-sm text-gray-600">Your Phone Number</label>
-          <input type="text" id="number" name="number" placeHolder="078..." className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+          <input type="text" id="number" name="number" placeholder="078..." className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
         </div>
       <div className="relative flex flex-col w-full h-20 justify-end">
         <div className="flex flex-row justify-between">
