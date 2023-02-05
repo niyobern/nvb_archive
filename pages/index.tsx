069eb45571e15a1bb88553940b2f65281ab43c1d
niyobern <niyobern@outlook.com>
2023-02-06 00:46:45 +0200

    done

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home({ data }: any) {
             px-6 py-10 sm:px-10 sm:py-6
             bg-white rounded-lg shadow-md lg:shadow-lg">
             <div className="text-center mb-4">
-                <h6 className="font-semibold text-[#063970] text-xl">GNC</h6>
+                <h6 className="font-semibold text-[#063970] text-xl">GNC Be Mine</h6>
                 <Image src={Logo} alt="image" className='w-10 md:w-14 h-10 md:h-14 lg:w-20 lg:h-20 justify-self-center mx-auto'/>
                 <div className="text-blue-900 text-lg font-bold">{data? data.message: ""}</div>
             </div>
