7fd46eb9e4a53b57eac5ee3384f916b0e9084224
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-10 14:33:28 +0000

    created app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app';
+import { withNextRuntime } from 'next-runtime/app';
+import { useState } from 'react';
+import axios from 'axios';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const globLinks = ["Documents", "Leave", "Users", "Payroll"]
+  const globPaths = ["/admin", "/leave", "/users", "/payroll"]
+  const [email, setEmail] = useState("")
+  function handleEmail(mail: any){
+    setEmail(mail)
+  }
+  return <Component {...pageProps}/>
+}
+
+export default MyApp;
