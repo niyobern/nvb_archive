bee629696097273896e1dd1ce3b6c9300ea0b009
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-11 17:07:33 +0000

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,14 @@
 export default function Footer(){
     return (
-        <div className="bg-gray-200 py-4 px-48 text-gray-800">
+        <div className="flex flex-row justify-between bg-gray-200 py-4 px-48">
+          <div className="text-gray-800">
             <span className="font-normal">Copyright © 2023 NVB. All Rights Reserved. </span>
             <span className="font-medium">Developed by Smaqui</span>
+          </div>
+          <div>
+            <svg className="h-6 w-6 text-gray-500"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M22 4.01c-1 .49-1.98.689-3 .99-1.121-1.265-2.783-1.335-4.38-.737S11.977 6.323 12 8v1c-3.245.083-6.135-1.395-8-4 0 0-4.182 7.433 4 11-1.872 1.247-3.739 2.088-6 2 3.308 1.803 6.913 2.423 10.034 1.517 3.58-1.04 6.522-3.723 7.651-7.742a13.84 13.84 0 0 0 .497 -3.753C20.18 7.773 21.692 5.25 22 4.009z" /></svg>
+            <svg className="h-8 w-8 text-gray-500"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" /></svg>
+          </div>
         </div>
     )
 }
