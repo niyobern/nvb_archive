2f72945ab8d46b878c8b892669952b42de9ef6fc
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-11 16:42:01 +0000

    Update footer.tsx and index.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,8 @@
 export default function Footer(){
     return (
-        <div>
+        <div className="bg-gray-200 py-4 px-48 text-gray-800">
+            <span className="font-normal">Copyright © 2023 NVB. All Rights Reserved. </span>
+            <span className="font-medium">Developed by Smaqui</span>
+        </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import NavBar from "../components/navbar"
 import Hero from "../components/hero"
+import Footer from "../components/footer"
 export default function Home(){
     return (
         <div>
             <NavBar/>
             <Hero/>
+            <Footer/>
         </div>
     )
 }
