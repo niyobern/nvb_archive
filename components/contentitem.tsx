5503223da004720bc4c1aa8a64f2fb22eec5e777
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-11 23:03:30 +0000

    Update 3 files

---
+++ components/contentitem.tsx
@@ -0,0 +1,10 @@
+export default function Content(){
+    return (
+        <div className="border border-gray-500 rounded p-2 md:p-4 flex flex-col">
+            <div className="flex flex-row justify-between">
+                <span className="text-lg font-medium">Introduction</span>
+                <span className="text-sm text-gray-700">2 hours remaining</span>
+            </div>
+        </div>
+    )
+}
--- components/lessonbanner.tsx
+++ components/lessonbanner.tsx
@@ -25,15 +25,15 @@ export default function Banner() {
                     <span> Sleeping</span>
                 </p>
             </div>
-            <div className="flex flex-row justify-between bg-green-800 py-4 px-2 md:px-4 text-white">
+            <div className="flex flex-row justify-between bg-gray-200 py-4 px-2 md:px-4">
                 <span>Intermediate</span>
                 <span>4 months</span>
                 <span>4.5</span>
                 <span>234 Reviews</span>
             </div>
             <div className="flex flex-row justify-between">
-                <div className="text-green-800 font-medium rounded px-4 py-2 border border-green-800">View details</div>
-                <div className="text-white font-medium px-4 py-2">Start Learning</div>
+                <div className="text-green-800 font-medium cursor-pointer rounded-sm px-5 py-2 border border-green-800 hover:bg-green-700 hover:text-white active:text-red-500">View details</div>
+                <div className="text-white rounded-sm font-medium cursor-pointer px-4 py-2 bg-green-800 hover:bg-green-700 hover:shadow active:text-red-500">Start Learning</div>
             </div>
         </div>
     )
