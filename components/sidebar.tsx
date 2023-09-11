a0dffcfca96ae314e10b4d822313f4c5e90e563a
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-11 23:52:23 +0000

    Update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,16 +1,17 @@
 export default function SideBar(){
     return (
-        <div className="w-1/4 bg-white py-4 border-x border-gray-400 h-screen">
+        <div className="w-1/4 bg-white py-4 h-screen">
             <ul className="px-4 flex flex-col gap-2 text-gray-800 font-normal">
-                <li>Introduction</li>
-                <li>API development with Gin</li>
-                <li>Frontend development with React</li>
-                <li>Introduction</li>
-                <li>API development with Gin</li>
-                <li>Frontend development with React</li>
-                <li>Introduction</li>
-                <li>API development with Gin</li>
-                <li>Frontend development with React</li>
+                <li className="text-lg font-bold">Lesson Title</li>
+                <li className="hover:text-lg hover:font-medium active:text-green-800 cursor-pointer">Introduction</li>
+                <li className="hover:text-lg hover:font-medium active:text-green-800 cursor-pointer">API development with Gin</li>
+                <li className="hover:text-lg hover:font-medium active:text-green-800 cursor-pointer">Frontend development with React</li>
+                <li className="hover:text-lg hover:font-medium active:text-green-800 cursor-pointer">Introduction</li>
+                <li className="hover:text-lg hover:font-medium active:text-green-800 cursor-pointer">API development with Gin</li>
+                <li className="hover:text-lg hover:font-medium active:text-green-800 cursor-pointer">Frontend development with React</li>
+                <li className="hover:text-lg hover:font-medium active:text-green-800 cursor-pointer">Introduction</li>
+                <li className="hover:text-lg hover:font-medium active:text-green-800 cursor-pointer">API development with Gin</li>
+                <li className="hover:text-lg hover:font-medium active:text-green-800 cursor-pointer">Frontend development with React</li>
             </ul>
         </div>
     )
