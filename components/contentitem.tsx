2dd9164c6f48ffb03ee6dddafbbf356b09433e2d
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-11 23:18:43 +0000

    Update contentitem.tsx

--- components/contentitem.tsx
+++ components/contentitem.tsx
@@ -1,10 +1,11 @@
 export default function Content(){
     return (
-        <div className="border border-gray-500 rounded p-2 md:p-4 flex flex-col">
+        <div className="border border-gray-500 rounded p-2 md:p-4 flex flex-col bg-white">
             <div className="flex flex-row justify-between">
                 <span className="text-lg font-medium">Introduction</span>
                 <span className="text-sm text-gray-700">2 hours remaining</span>
             </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
     )
 }
