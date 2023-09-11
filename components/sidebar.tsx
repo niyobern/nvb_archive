d4dc30fe77d15eb1e3b8f4cda0ca47df38efdd83
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-11 23:41:25 +0000

    Update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,7 +1,7 @@
 export default function SideBar(){
     return (
-        <div className="w-1/10 bg-white">
-            <ul>
+        <div className="w-1/4 bg-white py-4 border-x border-gray-400 h-screen">
+            <ul className="px-4 flex flex-col gap-2 text-gray-800 font-normal">
                 <li>Introduction</li>
                 <li>API development with Gin</li>
                 <li>Frontend development with React</li>
