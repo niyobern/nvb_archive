ab060bd419344936a23997f6ac6fa3e97084790c
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-11 23:26:59 +0000

    Update isomo.tsx

--- pages/isomo.tsx
+++ pages/isomo.tsx
@@ -1,12 +1,17 @@
 import Content from "../components/contentitem"
+import SideBar from "../components/sidebar"
 
 export default function amasomo(){
     return (
-        <div className="flex flex-col gap-4 bg-gray-100">
-            <Content/>
-            <Content/>
-            <Content/>
-            <Content/>
+        <div className="flex flex-row">
+            <SideBar/>
+            <div className="flex flex-col gap-4 bg-gray-100">
+                <Content/>
+                <Content/>
+                <Content/>
+                <Content/>
+            </div>
+            <SideBar/>
         </div>
     )
 }
