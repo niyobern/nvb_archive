7d2514a350935576efee44832f0309988103079f
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-11 23:06:04 +0000

    Update 2 files

--- pages/amasomo.tsx
+++ pages/amasomo.tsx
@@ -2,7 +2,7 @@ import Banner from "../components/lessonbanner"
 
 export default function amasomo(){
     return (
-        <div className="flex flex-row gap-4 flex-wrap mx-auto justify-center bg-gray-200">
+        <div className="flex flex-row gap-4 flex-wrap mx-auto justify-center bg-gray-100">
             <Banner/>
             <Banner/>
             <Banner/>
---
+++ pages/isomo.tsx
@@ -0,0 +1,12 @@
+import Content from "../components/contentitem"
+
+export default function amasomo(){
+    return (
+        <div className="flex flex-col gap-4 bg-gray-100">
+            <Content/>
+            <Content/>
+            <Content/>
+            <Content/>
+        </div>
+    )
+}
