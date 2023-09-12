c11cceee1b346c0d7ee5df608d587204471c2ccd
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-12 19:30:58 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,7 @@ import Footer from "../components/footer"
 export default function Home(){
     return (
         <div>
-            <NavBar/>
             <Hero/>
-            <Footer/>
         </div>
     )
 }
