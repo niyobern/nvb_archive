b3503b791f1434b3d6f69c94b3c501b3522f2e61
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-12 00:48:12 +0000

    Update lesson.tsx

--- components/lesson.tsx
+++ components/lesson.tsx
@@ -1,11 +1,13 @@
 export default function Lesson(){
     return (
         <div className="flex flex-col w-full">
-            <div className="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden pt-[56.25]">
+            <div className="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden">
               <iframe
                 className="embed-responsive-item absolute bottom-0 left-0 right-0 top-0 h-full w-full"
-                src="https://www.youtube.com/embed/vlDzYIIOYmM?enablejsapi=1&amp;origin=https%3A%2F%2Fmdbootstrap.com"
-                allowfullscreen=""
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/qlyUQaz8sH8?si=TTjiEglpHmJfalEq"
+                allowFullScreen={true}
                 data-gtm-yt-inspected-2340190_699="true"
                 id="240632615"></iframe>
             </div>
