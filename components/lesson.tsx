a29e9f33a0cf4ddd522780e58a19ea5475ce7bed
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-12 00:41:58 +0000

    Update lesson.tsx and classroom.tsx

--- components/lesson.tsx
+++ components/lesson.tsx
@@ -1,7 +1,14 @@
 export default function Lesson(){
     return (
-        <div className="flex flex-col">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/qlyUQaz8sH8?si=rao6nb4daE6xA4W3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <div className="flex flex-col w-full">
+            <div className="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden pt-[56.25]">
+              <iframe
+                className="embed-responsive-item absolute bottom-0 left-0 right-0 top-0 h-full w-full"
+                src="https://www.youtube.com/embed/vlDzYIIOYmM?enablejsapi=1&amp;origin=https%3A%2F%2Fmdbootstrap.com"
+                allowfullscreen=""
+                data-gtm-yt-inspected-2340190_699="true"
+                id="240632615"></iframe>
+            </div>
             <div>
                 <span>Heading one</span>
                 <p>Contents are here</p>
--- pages/classroom.tsx
+++ pages/classroom.tsx
@@ -2,7 +2,7 @@ import Lesson from "../components/lesson"
 import SideBar from "../components/sidebar"
 export default function Class(){
     return (
-        <div className="flex flex-row justify between">
+        <div className="flex flex-row justify-between">
             <SideBar/>
             <Lesson/>
             <SideBar/>
