a1039ace9fa8bc14e98967a2e5f4af6746795dc1
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-12 00:18:44 +0000

    Update lesson.tsx

--- components/lesson.tsx
+++ components/lesson.tsx
@@ -1,7 +1,7 @@
 export default function Lesson(){
     return (
-        <div className="flex flex-col">
-            <iframe width="720" height="720" className="h-full w-full" src="https://www.youtube.com/embed/OrEz65gPfJo?si=GFWWUcYDG9PV-P-l" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <div className="flex flex-col w-full">
+            <iframe width="840" height="472" src="https://www.youtube.com/embed/OrEz65gPfJo?si=rpyBnq0e9zxycNiU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
     )
 }
