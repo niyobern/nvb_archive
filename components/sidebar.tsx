6ff579bb160bfd09fbe1eb52a5f5fcd80c06b70b
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-12 09:02:32 +0000

    Update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,6 @@
 export default function SideBar(){
     return (
-        <div className="w-1/4 bg-white py-4 h-screen">
+        <div className="w-1/2 bg-white py-4 h-screen">
             <ul className="pl-4 pr-2 flex flex-col gap-2 text-gray-800 font-normal">
                 <li className="text-lg font-bold">Lesson Title</li>
                 <li className="hover:text-lg hover:font-medium active:text-green-800 cursor-pointer">Introduction</li>
