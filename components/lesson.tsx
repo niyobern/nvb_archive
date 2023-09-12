2137b5b5b1ac29b999f08aad506399160b461436
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-12 00:08:41 +0000

    Update lesson.tsx and classroom.tsx

--- components/lesson.tsx
+++ components/lesson.tsx
@@ -1,7 +1,7 @@
 export default function Lesson(){
     return (
         <div className="flex flex-col">
-            <iframe width="100%" height="100%" className="h-full w-full" src="https://www.youtube.com/embed/OrEz65gPfJo?si=GFWWUcYDG9PV-P-l" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <iframe width="720" height="720" className="h-full w-full" src="https://www.youtube.com/embed/OrEz65gPfJo?si=GFWWUcYDG9PV-P-l" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
     )
 }
--- pages/classroom.tsx
+++ pages/classroom.tsx
@@ -1,8 +1,11 @@
 import Lesson from "../components/lesson"
+import SideBar from "../components/sidebar"
 export default function Class(){
     return (
-        <div className="white">
+        <div className="flex flex-row">
+            <SideBar/>
             <Lesson/>
+            <SideBar/>
         </div>
     )
 }
