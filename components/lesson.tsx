ca9429193bfa345b0c58967907be8dc500421705
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-12 00:56:19 +0000

    Update lesson.tsx and tailwind.config.js

--- components/lesson.tsx
+++ components/lesson.tsx
@@ -1,15 +1,8 @@
 export default function Lesson(){
     return (
         <div className="flex flex-col w-full">
-            <div className="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden">
-              <iframe
-                className="embed-responsive-item absolute bottom-0 left-0 right-0 top-0 h-full w-full"
-                width="560"
-                height="315"
-                src="https://www.youtube.com/embed/qlyUQaz8sH8?si=TTjiEglpHmJfalEq"
-                allowFullScreen={true}
-                data-gtm-yt-inspected-2340190_699="true"
-                id="240632615"></iframe>
+            <div className="aspect-w-16 aspect-h-9">
+              <iframe src="https://www.youtube.com/embed/XiuRH7i022g" frameborder="0" allowfullscreen></iframe>
             </div>
             <div>
                 <span>Heading one</span>
