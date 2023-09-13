c26b947018d613b309552de8866e8bd90c9d3deb
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-13 08:45:46 +0000

    Update amasomo.tsx

--- pages/amasomo.tsx
+++ pages/amasomo.tsx
@@ -2,7 +2,7 @@ import Banner from "../components/lessonbanner"
 
 export default function amasomo(){
     return (
-        <div className="grid grid-cols-2 gap-4 bg-gray-100 p-2 shadow-l">
+        <div className="grid grid-cols-2 gap-4 bg-gray-100 p-2">
             <Banner/>
             <Banner/>
             <Banner/>
