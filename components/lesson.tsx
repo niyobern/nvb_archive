6ec38684584e5570797709da8d55c4c32d3cda40
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-13 08:26:33 +0000

    Update lesson.tsx

--- components/lesson.tsx
+++ components/lesson.tsx
@@ -5,9 +5,9 @@ export default function Lesson(){
               <iframe src="https://www.youtube.com/embed/XiuRH7i022g" allowFullScreen={true}></iframe>
             </div>
             <div className="flex flex-col gap-2 bg-white">
-                <span className="text-2xl font-semibold">Heading one</span>
+                <span className="text-2xl font-bold">Heading one</span>
                 <span className="text-xl font-medium">Heading 2</span>
-                <p className="leading-none text-justify">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <p className="leading-tight text-justify">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
             </div>
         </div>
     )
