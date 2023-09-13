7baf6763e4c6bd34882c49ecafe4f3391754f355
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-13 22:12:15 +0000

    Update quizindicator.tsx and ibazwa.tsx

--- components/quizindicator.tsx
+++ components/quizindicator.tsx
@@ -1,12 +1,22 @@
-export default function Indicator(){
+import { useState, useEffect } from "react"
+export default function Indicator({ start }: any){
+    const duration = 20*1000
+    const [current, setCurrent] = useState(Date.now())
+    const elapsed = current - start
+    const percentage = elapsed*100 / duration
+    useEffect(
+        () => {
+            setCurrent(Date.now())
+        }, [current]
+    )
     return (
         <div className="flex flex-col">
             <div className="flex flex-row justify-between">
-                <div><span>Time Left:</span> <span>0:12:56</span></div>
-                <div><span>Total Time:</span> <span>0:20:00</span></div>
+                <div><span className="text-green-700 font-medium">Total Time:</span> <span className="text-green-700 txt-lg font-semibold">0:20:00</span></div>
+                <div><span className="text-blue-700 font-medium">Time Left:</span> <span className="text-blue-700 text-lg font-semibold">0:12:56</span></div>
             </div>
-            <div className="w-full h-8 bg-red-600">
-                <span className="bg-blue-600 h-full w-[23%]"></span>
+            <div className="w-full h-4 bg-sky-500 relative">
+                <div className="bg-red-600 h-full z-70 text-white" style={{width: `${percentage}%`}}></div>
             </div>
         </div>
     )
--- pages/ibazwa.tsx
+++ pages/ibazwa.tsx
@@ -2,9 +2,10 @@ import Question from "../components/questtion"
 import Indicator from "../components/quizindicator"
 
 export default function Ibazwa(){
+    const start = Date.now()
     return (
         <div>
-            <Indicator/>
+            <Indicator start={start}/>
             <Question/>
         </div>
     )
