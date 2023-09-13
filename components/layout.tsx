c417ab6300f75b93b31c72d342715e7f5d6da231
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-13 08:08:11 +0000

    Update layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,20 @@
+import { useRouter } from "next/router"
 import SideBar from "./sidebar"
 import NavBar from "./navbar"
 import Footer from "./footer"
 export default function Layout({children}: any){
+    const router = useRouter()
+    const route = router.pathname
+    console.log(route)
+    if (route === "/"){
+        return (
+            <div>
+                <NavBar/>
+                {children}
+                <Footer/>
+            </div>
+        )
+    }
     return (
         <div className="flex flex-col h-screen">
             <NavBar/>
