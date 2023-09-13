7b01a49154b499d03f56aeb2f68436cff1157aca
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-13 22:58:22 +0000

    Update 3 files

---
+++ components/timeindicator.tsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react"
+export default function Indicator({ start, duration }: any){
+    function msToTime(s: any) {
+
+        // Pad to 2 or 3 digits, default is 2
+        function pad(n: any, z: any) {
+          z = z || 2;
+          return ('00' + n).slice(-z);
+        }
+      
+        var ms = s % 1000;
+        s = (s - ms) / 1000;
+        var secs = s % 60;
+        s = (s - secs) / 60;
+        var mins = s % 60;
+        var hrs = (s - mins) / 60;
+      
+        return pad(hrs, 2) + ':' + pad(mins, 2) + ':' + pad(secs, 2);
+      }
+      
+      console.log(msToTime(55018))
+    const [percentage, setPercentage] = useState(0)
+    useEffect(
+        () => {
+            setTimeout( () => {
+                const current = Date.now();
+                const elapsed = current - start;
+                setPercentage(elapsed*100 / duration)
+            }, 1000)
+        }
+    )
+    return (
+        <div className="flex flex-col">
+            <div className="flex flex-row justify-between">
+                <div><span className="text-green-700 font-medium">Total Time:</span> <span className="text-green-700 txt-lg font-semibold">{msToTime(duration)}</span></div>
+                <div><span className="text-blue-700 font-medium">Time Left:</span> <span className="text-blue-700 text-lg font-semibold">{msToTime(duration - (percentage*duration/100))}</span></div>
+            </div>
+            <div className="w-full h-4 bg-sky-500 relative">
+                <div className="bg-red-600 h-full z-70 text-white" style={{width: `${percentage}%`}}></div>
+            </div>
+        </div>
+    )
+}
--- pages/ibazwa.tsx
+++ pages/ibazwa.tsx
@@ -1,11 +1,12 @@
 import Question from "../components/questtion"
-import Indicator from "../components/quizindicator"
+import Indicator from "../components/timeindicator"
 
 export default function Ibazwa(){
     const start = Date.now()
+    const duration = 20*60*1000
     return (
         <div>
-            <Indicator start={start}/>
+            <Indicator start={start} duration={duration}/>
             <Question/>
         </div>
     )
