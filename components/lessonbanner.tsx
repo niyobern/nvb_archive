1d50f3a054c921deeb4db2a8b05a1bc324ada51f
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-13 09:04:40 +0000

    Update lessonbanner.tsx and navbar.tsx

--- components/lessonbanner.tsx
+++ components/lessonbanner.tsx
@@ -1,7 +1,7 @@
 export default function Banner() {
     return (
         <div className="flex flex-col pt-4 gap-2 rounded-sm bg-white w-full pb-2 shadow-lg shadow-gray-200">
-            <div className="text-3xl font-semibold px-2 md:px-4 text-green-800">Lorem ipsum dolor sit amet</div>
+            <div className="text-3xl font-semibold px-2 md:px-4 text-teal-800">Lorem ipsum dolor sit amet</div>
             <p className="text-md text-gray-700 px-2 md:px-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             <div className="p-2">
                 <span className="text-xl font-medium pb-2">Prerequisites</span>
@@ -32,8 +32,8 @@ export default function Banner() {
                 <span>234 Reviews</span>
             </div>
             <div className="flex flex-row justify-between">
-                <div className="text-green-800 font-medium cursor-pointer rounded-sm px-5 py-2 border border-green-800 hover:bg-green-700 hover:text-white active:text-red-500">View details</div>
-                <div className="text-white rounded-sm font-medium cursor-pointer px-4 py-2 bg-green-800 hover:bg-green-700 hover:shadow active:text-red-500">Start Learning</div>
+                <div className="text-teal-800 font-medium cursor-pointer rounded-sm px-5 py-2 border border-teal-800 hover:bg-teal-700 hover:text-white active:text-red-500">View details</div>
+                <div className="text-white rounded-sm font-medium cursor-pointer px-4 py-2 bg-teal-800 hover:bg-teal-700 hover:shadow active:text-red-500">Start Learning</div>
             </div>
         </div>
     )
