4b18595ac77b5c38d1ae8e27b2a328412b0e7066
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-18 22:50:48 +0000

    Update add.tsx and upload.tsx

--- pages/add.tsx
+++ pages/add.tsx
@@ -1,5 +1,5 @@
 export default function Add(){
     return (
-    <div>div</div>
+    <div className="bg-blue-400 h-48 w-48">div</div>
     )
 }
--- pages/upload.tsx
+++ pages/upload.tsx
@@ -1,8 +1,8 @@
 import Add from "../components/add"
 export default  function Upload(){
     return (
-        <div className="bg-blue-400 h-48 w-48">
-            <add/>
+        <div>
+            <Add/>
         </div>
     )
 }
