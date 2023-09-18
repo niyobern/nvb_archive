d57641d65867d5a2fbda0bb1f38deeed3296bbcf
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-18 16:17:27 +0000

    Update questioncount.tsx

--- components/questioncount.tsx
+++ components/questioncount.tsx
@@ -13,7 +13,7 @@ export default function Count(){
     return (
         <div className="flex flex-row flex-wrap">
             {qdone.map( (value, index) => (
-                <div className={`${value ? "bg-green-700" : "bg-green-400"} w-12 h-6 border border-gray-200`} key={index}></div>
+                <div onClick={(index) => handleDone(index)} className={`${value ? "bg-green-700" : "bg-green-400"} w-12 h-6 border border-gray-200`} key={index}></div>
             ))}
         </div>
     )
