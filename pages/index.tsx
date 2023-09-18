e9ee8181cf8c5b3530400ac2eee52e539cfc904c
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-18 16:54:59 +0000

    Update 3 files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
-import NavBar from "../components/navbar"
 import Hero from "../components/hero"
-import Footer from "../components/footer"
 export default function Home(){
     return (
         <div>
