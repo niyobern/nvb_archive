e59e1a0ab24b7e5b55c2f7a8022bbd682437ae40
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-18 17:58:31 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,10 @@ import axios from "axios"
 import { useEffect } from "react"
 export default function Home(){
     useEffect(() => {
-        axios.post("https://drive.deta.sh/v1/collection-key-silkworm-88o/drive", {headers: {"X-API-Key": "c0j5ubhk5sb_5Y1Qi9XAMj5sM5cZ3tpRoKF55TgJepdV"}})
+        axios.get("https://drive.deta.sh/v1/collection-key-silkworm-88o/drive/files", {headers: {"X-API-Key": "c0j5ubhk5sb_5Y1Qi9XAMj5sM5cZ3tpRoKF55TgJepdV"}})
+        .then((res)=> {
+            console.log(res)
+        })
     })
     return (
         <div>
