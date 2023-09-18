651554af5f32366aab1a930fc2f03f8f6e3cd9d0
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-18 19:27:37 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,7 @@ import { useEffect } from "react"
 export default function Home(){
     useEffect(() => {
         axios.get("https://drive.deta.sh/v1/collection-key-silkworm-88o/drive/files", {headers: {"X-API-Key": "c0j5ubhk5sb_5Y1Qi9XAMj5sM5cZ3tpRoKF55TgJepdV"}})
-        .then((res)=> {
-            console.log(res)
-        })
+        .then(res=> console.log(res))
     })
     return (
         <div>
