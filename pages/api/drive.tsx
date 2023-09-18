f1054dae5b94159e46494d90513b68d54c788f98
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-18 20:13:03 +0000

    Update 3 files

--- pages/api/drive.tsx
+++ pages/api/drive.tsx
@@ -1,11 +1,28 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import { Deta } from "deta"
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse){
+export default async function handler(req: NextApiRequest, res: NextApiResponse){    
+    const deta = Deta(); //instantiate with Data Key or env DETA_PROJECT_KEY
+    
+    const db = deta.Base("simple_db");
+    await db.put({ name: "alex", age: 77 });
+    // We will use "one" as a key.
+    await db.put({ name: "alex", age: 77 }, "one");
+    // The key could also be included in the object itself.
+    await db.put({ name: "alex", age: 77, key: "one" });
 
-    const deta = Deta("collection-key-silkworm-88o", "c0j5ubhk5sb_5Y1Qi9XAMj5sM5cZ3tpRoKF55TgJepdV")
-    const drive = deta.Drive("drive")
-    drive.put("image", {data: "Try everything and go along"})
-    // const resp = await drive.get("image")
-    res.end("Okay")
+    // Store simple types.
+    await db.put("hello, worlds");
+    await db.put(7);
+    // "success" is the value and "smart_work" is the key.
+    await db.put("success", "smart_work");
+    await db.put(["a", "b", "c"], "my_abc");
+    
+    // Put expiring items.
+    // Expire in 300 seconds.
+    await db.put({ name: "alex", age: 21 }, "alex21", { expireIn: 300 });
+    // Expire at date.
+    await db.put({ name: "max", age: 28 }, "max28", { expireAt: new Date("2023-01-01T00:00:00") });
+    const item = await db.get('one');
+    res.end(item)
 }
