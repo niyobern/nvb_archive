31bb002a66fb58638204a7550724660bbf061ee7
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-18 20:39:57 +0000

    Update drive.tsx and index.tsx

--- pages/api/drive.tsx
+++ pages/api/drive.tsx
@@ -2,27 +2,27 @@ import { NextApiRequest, NextApiResponse } from "next"
 import { Deta } from "deta"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse){    
-    const deta = Deta(); //instantiate with Data Key or env DETA_PROJECT_KEY
+    const deta = Deta("c0j5ubhk5sb_dSpm3XKufpv54g1JMYBjH4sco9TqbR9y"); //instantiate with Data Key or env DETA_PROJECT_KEY
     
     const db = deta.Base("simple_db");
     await db.put({ name: "alex", age: 77 });
-    // We will use "one" as a key.
-    await db.put({ name: "alex", age: 77 }, "one");
-    // The key could also be included in the object itself.
-    await db.put({ name: "alex", age: 77, key: "one" });
+    // // We will use "one" as a key.
+    // await db.put({ name: "alex", age: 77 }, "one");
+    // // The key could also be included in the object itself.
+    // await db.put({ name: "alex", age: 77, key: "one" });
 
-    // Store simple types.
-    await db.put("hello, worlds");
-    await db.put(7);
-    // "success" is the value and "smart_work" is the key.
-    await db.put("success", "smart_work");
-    await db.put(["a", "b", "c"], "my_abc");
+    // // Store simple types.
+    // await db.put("hello, worlds");
+    // await db.put(7);
+    // // "success" is the value and "smart_work" is the key.
+    // await db.put("success", "smart_work");
+    // await db.put(["a", "b", "c"], "my_abc");
     
-    // Put expiring items.
-    // Expire in 300 seconds.
-    await db.put({ name: "alex", age: 21 }, "alex21", { expireIn: 300 });
-    // Expire at date.
-    await db.put({ name: "max", age: 28 }, "max28", { expireAt: new Date("2023-01-01T00:00:00") });
-    const item = await db.get('one');
-    res.end(item)
+    // // Put expiring items.
+    // // Expire in 300 seconds.
+    // await db.put({ name: "alex", age: 21 }, "alex21", { expireIn: 300 });
+    // // Expire at date.
+    // await db.put({ name: "max", age: 28 }, "max28", { expireAt: new Date("2023-01-01T00:00:00") });
+    // const item = await db.get('one');
+    res.end("item")
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,10 @@ import Hero from "../components/hero"
 import axios from "axios"
 import { useEffect } from "react"
 export default function Home(){
-    useEffect(() => {
-        axios.get("https://drive.deta.sh/v1/collection-key-silkworm-88o/drive/files", {headers: {"X-API-Key": "c0j5ubhk5sb_5Y1Qi9XAMj5sM5cZ3tpRoKF55TgJepdV"}})
-        .then(res=> console.log(res))
-    })
+    // useEffect(() => {
+    //     axios.get("https://drive.deta.sh/v1/collection-key-silkworm-88o/drive/files", {headers: {"X-API-Key": "c0j5ubhk5sb_5Y1Qi9XAMj5sM5cZ3tpRoKF55TgJepdV"}})
+    //     .then(res=> console.log(res))
+    // })
     return (
         <div>
             <Hero/>
