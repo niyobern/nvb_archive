35f63fe1e0d2d2f2ab269a2200b38291916fc7b7
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-19 17:50:59 +0000

    Update add.tsx

--- components/add.tsx
+++ components/add.tsx
@@ -1,5 +1,12 @@
 export default function Add(){
     return (
-    <div className="bg-blue-400 h-48 w-48">div</div>
+    <div className="bg-blue-400 h-full w-full rounded shadow shadow-blue-400">
+        <form className="flex flex-col gap-2 m-12">
+            <label htmlFor="title" className="text-lg font-medium font-teal-800">Section Title</label>
+            <input type="text" id="fname" name="title" className="rounded p-2"/>
+            <label htmlFor="w3review">Review of W3Schools:</label>
+            <textarea id="w3review" name="w3review"></textarea>
+        </form>
+    </div>
     )
 }
