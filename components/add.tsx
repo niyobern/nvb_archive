750316a08085cfdef7b168d86ad61ec82a93266a
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-19 21:37:38 +0000

    Update add.tsx

--- components/add.tsx
+++ components/add.tsx
@@ -11,6 +11,12 @@ export default function Add(){
       setFile(e.target.files[0]);
     }
   };
+  const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value)
+  };
+  const handleContentChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.target.value)
+  };
 
   const handleUploadClick = () => {
     if (!file || title === "" || content === "") {
@@ -30,9 +36,9 @@ export default function Add(){
     <div className="bg-blue-400 h-full w-full rounded shadow shadow-blue-400">
         <form className="flex flex-col gap-2 m-12">
             <label htmlFor="title" className="text-lg font-medium font-teal-800">Section Title</label>
-            <input type="text" id="title" name="title" className="rounded p-2"/>
+            <input type="text" id="title" name="title" className="rounded p-2" onChange={handleTitleChange} value={title}/>
             <label htmlFor="content">Section Content:</label>
-            <textarea id="content" name="content" className="h-48 w-full"/>
+            <textarea id="content" name="content" className="h-48 w-full" onChange={handleContentChange} value={content}/>
             <div className="flex flex-col">
               <input type="file" onChange={handleFileChange} name="file" id="file"/>
               <div>{file && `${file.name} - ${file.type}`}</div>
