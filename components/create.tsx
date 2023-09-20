ef704f860f856f7365e6deb96dd0195315de066e
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-20 13:52:17 +0000

    Update create.tsx and upload.tsx

--- components/create.tsx
+++ components/create.tsx
@@ -30,12 +30,12 @@ export default function Create(){
     return (
     <div className="bg-blue-400 h-full w-full rounded shadow shadow-blue-400 flex flex-col content-center">
         <h1 className='text-white text-2xl font-bold text-center mt-2'>New Lesson</h1>
-        <form className="flex flex-col gap-2 m-12" onSubmit={handleUploadClick}>
+        <form className="flex flex-col gap-4 m-12" onSubmit={handleUploadClick}>
             <label htmlFor="title" className="text-lg font-medium">Lesson Title</label>
             <input type="text" id="title" name="title" className="rounded p-2" onChange={handleTitleChange} value={title}/>
             <label htmlFor="description" className="text-lg font-medium">Lesson Description</label>
             <textarea id="description" name="description" className="h-48 w-full rounded" onChange={handleDescriptionChange} value={description}/>
-            <button type='submit' className="bg-gray-200 text-xl font-medium rounded p-2 hover:shadow-lg hover:bg-teal-200 hover:shadow-gray-500">Upload</button>
+            <button type='submit' className="bg-gray-200 text-xl font-medium rounded p-2 hover:shadow-lg hover:bg-teal-200 hover:shadow-gray-500">Create</button>
         </form>
     </div>
     )
--- pages/upload.tsx
+++ pages/upload.tsx
@@ -1,8 +1,11 @@
 import Add from "../components/add"
+import Create from "../components/create"
+
 export default  function Upload(){
     return (
         <div className="bg-blue-400 h-full w-full rounded shadow shadow-blue-400 flex flex-col justif-center content-center">
-            <Add lesson_key="cffdrts" lesson_title="Title of Lesson"/>
+            <Create/>
+            {/* <Add lesson_key="cffdrts" lesson_title="Title of Lesson"/> */}
         </div>
     )
 }
