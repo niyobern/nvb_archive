e64f7cd4c48c8d09c63cb4ac3d5fdc3abbd891dd
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-20 12:41:06 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports= {
   images: {
-    domains: ["https://reponv-1-d0312461.deta.app"]
+    domains: ["reponv-1-d0312461.deta.app"]
   },
   reactStrictMode: true,
 }
