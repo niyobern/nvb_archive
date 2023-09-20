a2e6c8decffde12ff49e2d0e5eda819bf5fefcc7
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-20 13:56:14 +0000

    Update upload.tsx

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -4,8 +4,8 @@ import Create from "../components/create"
 export default  function Upload(){
     return (
         <div className="bg-blue-400 h-full w-full rounded shadow shadow-blue-400 flex flex-col justif-center content-center">
-            <Create/>
-            {/* <Add lesson_key="cffdrts" lesson_title="Title of Lesson"/> */}
+            {/* <Create/> */}
+            <Add lesson_key="cffdrts" lesson_title="Title of Lesson"/>
         </div>
     )
 }
