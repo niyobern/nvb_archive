8d4a26e2825d3fa50642e55d146ace62b8761e35
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-21 10:52:56 +0000

    Update card.tsx and next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports= {
   images: {
-    domains: ["reponv-1-d0312461.deta.app"]
+    domains: ["nvb_backend-1-z3745144.deta.app"]
   },
   reactStrictMode: true,
 }
