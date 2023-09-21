8fbdd807fb5b5d3270f28351fdd785dc4da4c272
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-21 10:13:56 +0000

    Update 2 files

--- pages/incamake.tsx
+++ pages/incamake.tsx
@@ -1,5 +1,5 @@
 import Lesson from "../components/lesson"
-export default function Class(){
+export default function Incamake(){
     return (
         <div className="p-1 h-full flex flex-col">
             <Lesson/>
