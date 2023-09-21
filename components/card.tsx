42e2d08a568822b67207f56a8a93d493000b4e14
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-21 10:38:46 +0000

    Update card.tsx

--- components/card.tsx
+++ components/card.tsx
@@ -4,9 +4,17 @@ export default function Card() {
             <div className="rounded bg-white shadow-lg shadow-gray-400 p-4 mx-auto flex flex-col">
                 <p className="text-justify text-lg text-gray-800">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 <div className="flex flex-col gap-2 mt-2">
-                    <div className="flex flex-col gap-2 justify-start">
-                        <svg className="h-8 w-8 text-teal-800"  fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"/></svg>
-                        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</span>
+                    <div className="flex flex-row gap-2 justify-start">
+                        <svg className="h-6 w-6 text-teal-800"  fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"/></svg>
+                        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+                    </div>
+                    <div className="flex flex-row gap-2 justify-start">
+                        <svg className="h-6 w-6 text-teal-800"  fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"/></svg>
+                        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+                    </div>
+                    <div className="flex flex-row gap-2 justify-start">
+                        <svg className="h-6 w-6 text-teal-800"  fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"/></svg>
+                        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
                     </div>
                 </div>
             </div>
