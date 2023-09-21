f79503108a5bebd77b8ffc76ba53dbd94776adc9
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-21 10:50:53 +0000

    Update card.tsx

--- components/card.tsx
+++ components/card.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image"
 export default function Card() {
     return (
         <div className="bg-teal-100 h-screen flex flex-col justify-center content-center px-10">
             <div className="rounded bg-white shadow-lg shadow-gray-400 p-4 mx-auto flex flex-col">
-                <p className="text-justify text-lg text-gray-800">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p className="text-justify text-lg text-gray-800 md:py-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <Image src="https://nvb_backend-1-z3745144.deta.app/image" alt="Image" height={500} width={500} className="h-48 rounded-sm"/>
                 <div className="flex flex-col gap-2 mt-2">
                     <div className="flex flex-row gap-2 justify-start">
                         <svg className="h-6 w-6 text-teal-800"  fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"/></svg>
