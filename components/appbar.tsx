fd303c26891b75420b63e66c8efd4056a4db0f9d
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-24 02:03:10 +0000

    Update appbar.tsx

--- components/appbar.tsx
+++ components/appbar.tsx
@@ -40,7 +40,7 @@ export default function AppBar(){
               <span className={`fi fi-${lang} text-2xl`}/>
             </div>
             <Link href="/" passHref={true}><span className="text-3xl font-bold text-teal-800 leading-none antialiased uppercase">NVB</span></Link>
-            <Link passHref={true} href="/testimonials" className="border-2 hover:border-none border-teal-800 rounded-full group flex relative justify-center self-center hover:self-end items-center">
+            <Link passHref={true} href="/account" className="border-2 hover:border-none border-teal-800 rounded-full group flex relative justify-center self-center hover:self-end items-center">
               <div className="hidden group-hover:flex bg-white font-bold text-sky-900 py-1">Konti Yanjye</div>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-7 w-7 text-teal-700"><path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" /></svg>
             </Link>
