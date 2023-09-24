ce7e7fcab699d84fa315cc197d536638abc19f17
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-24 00:52:29 +0000

    Update contentitem.tsx

--- components/contentitem.tsx
+++ components/contentitem.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link"
 
-export default function Content({ title, description, time, id, link }: any){
+export default function Content({ title, description, time, id, link, current }: any){
     if (id){
         return (
             <Link href={`/isomo/${id}`}>
-                <div className="border border-gray-500 rounded p-2 md:p-4 flex flex-col bg-white">
+                <div className={`border border-gray-500 rounded p-2 md:p-4 flex flex-col ${current ? "bg-teal-600": "bg-white"}`}>
                     <div className="flex flex-row justify-between">
                         <span className="text-lg font-medium">{title}</span>
                         {time && <span className="text-sm text-gray-700">{time} remaining</span>}
