5e4a70259d3bf856318fdc1a44e55ec4d63f2bd8
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-09-24 01:01:54 +0000

    Update contentitem.tsx and [igice].tsx

--- pages/isomo/[igice].tsx
+++ pages/isomo/[igice].tsx
@@ -5,7 +5,7 @@ export default function Igice(){
         <div className="flex flex-col gap-4 bg-gray-100 p-4 h-full">
             <Content link="/" title="Introduction" time="23 cards"/>
             <Content link="/" title="Introduction" time="1 cards"/>
-            <Content link="/" title="Introduction" time="13 cards" current={true}/>
+            <Content current={true} link="/" title="Introduction" time="13 cards"/>
             <Content link="/" title="Introduction" time="0 cards"/>
         </div>
     )
