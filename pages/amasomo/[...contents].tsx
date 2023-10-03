6afc721f099cc6bfe8030f3c4fd775f03617bae9
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-10-03 19:56:31 +0000

    refactored

--- pages/amasomo/[...contents].tsx
+++ pages/amasomo/[...contents].tsx
@@ -1,12 +1,12 @@
 import Content from "../../components/contentitem"
-import { usePathname, useSearchParams } from 'next/navigation'
+// import { usePathname, useSearchParams } from 'next/navigation'
 
 export default function contents(){
-    const url = "https://nvb_backend-1-z3745144.deta.app"
-    const router = usePathname()
-    const route = router?.split("/")
-    const search = useSearchParams()
-    console.log(search.get("geted"))
+    // const url = "https://nvb_backend-1-z3745144.deta.app"
+    // const router = usePathname()
+    // const route = router?.split("/")
+    // const search = useSearchParams()
+    // console.log(search.get("geted"))
     // console.log(route)
     return (
         <div className="flex flex-col gap-4 bg-gray-100 p-4 h-full">
