412081a0b974b73a0ebc3be70aebe4ae750c0b21
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-10-04 23:44:00 +0000

    updated

--- pages/amasomo/[...contents].tsx
+++ pages/amasomo/[...contents].tsx
@@ -10,13 +10,13 @@ import { __String } from "typescript"
 //   }
    
   export const getStaticPaths = (async () => {
-    const lessons = (await axios.get("https://nvb_backend-1-z3745144.deta.app/lesson")).data._items.map((item: any) => "/amasomo/"+ item.key)
-    console.log(lessons)
+    const lessonKeys = (await axios.get("https://nvb_backend-1-z3745144.deta.app/lesson")).data._items.map((item: any) => item.key)
+    const lessons = lessonKeys.map((item: any) => "/amasomo/"+ item)
     const contents: string[] = []
-    lessons.forEach((value: any)=>{
+    lessonKeys.forEach((value: any)=>{
         axios.get(`://nvb_backend-1-z3745144.deta.app/lesson/content?lesson_id=${value}`)
         .then((item)=>{
-            contents.push(item.data.key)
+            contents.push(item.data._items.map((item: any) => item.key))
         })
     })
     return {
@@ -26,12 +26,13 @@ import { __String } from "typescript"
   }) satisfies GetStaticPaths
    
   export const getStaticProps = (async (context) => {
-    const lessons = (await axios.get("https://nvb_backend-1-z3745144.deta.app/lesson/")).data
+    const lessons = (await axios.get("https://nvb_backend-1-z3745144.deta.app/lesson/")).data._items.map((item: any) => item)
+    const lessonKeys = lessons.map((item: any) => item.key)
     const contents: string[] = []
     lessons.forEach((value: any)=>{
         axios.get(`://nvb_backend-1-z3745144.deta.app/lesson/content?lesson_id=${value}`)
         .then((item)=>{
-            contents.push(item.data)
+            contents.push(item.data._items.map((item: any) => item))
         })
     })
     return { props: { lessons: lessons, contents: contents} }
@@ -40,8 +41,8 @@ import { __String } from "typescript"
   }>
    
   export default function Contents({ lessons, contents }: InferGetStaticPropsType<typeof getStaticProps>) {
-    console.log(lessons)
-    console.log(contents)
+    // console.log(lessons)
+    // console.log(contents)
     return (
         <div className="flex flex-col gap-4 bg-gray-100 p-4 h-full">
             <Content id="1" current={true} time="40 cards" title="Introduction" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."/>
