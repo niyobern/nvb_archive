c954ee8b52f0f835aabfe26b3f3f89afd2584fd0
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-10-05 00:56:08 +0000

    updated

--- pages/amasomo/[...contents].tsx
+++ pages/amasomo/[...contents].tsx
@@ -14,7 +14,7 @@ import { __String } from "typescript"
     const lessons = lessonKeys.map((item: any) => "/amasomo/"+ item)
     const contents: string[] = []
     lessonKeys.forEach((value: any)=>{
-        axios.get(`://nvb_backend-1-z3745144.deta.app/lesson/content?lesson_id=${value}`)
+        axios.get(`https://nvb_backend-1-z3745144.deta.app/lesson/content?lesson_id=${value}`)
         .then((item)=>{
             contents.push(item.data._items.map((item: any) => item.key))
         })
@@ -25,24 +25,22 @@ import { __String } from "typescript"
     }
   }) satisfies GetStaticPaths
    
-  export const getStaticProps = (async (context) => {
-    const lessons = (await axios.get("https://nvb_backend-1-z3745144.deta.app/lesson/")).data._items.map((item: any) => item)
+  export const getStaticProps = (async () => {
+    const lessons = (await axios.get("https://nvb_backend-1-z3745144.deta.app/lesson/")).data._items.map((item: any) => ({...item, "contents": []}))
     const lessonKeys = lessons.map((item: any) => item.key)
-    const contents: string[] = []
-    lessonKeys.forEach((value: any)=>{
-        axios.get(`://nvb_backend-1-z3745144.deta.app/lesson/content?lesson_id=${value}`)
+    const contents :any[] = ["a", "non"]
+    for (let i of lessonKeys){
+        axios.get(`https://nvb_backend-1-z3745144.deta.app/lesson/content?lesson_id=${i}`)
         .then((item)=>{
-            contents.push(item.data._items.map((item: any) => item))
+          item.data._items.map((i: any) => contents.push(i))
         })
-    })
-    return { props: { lessons: lessons, contents: contents} }
-  }) satisfies GetStaticProps<{
-    lessons: {}, contents: {}
-  }>
+    }
+    console.log(contents)
+    return { props: { lessons: contents } }
+  })
    
-  export default function Contents({ lessons, contents }: InferGetStaticPropsType<typeof getStaticProps>) {
-    // console.log(lessons)
-    // console.log(contents)
+  export default function Contents({ lessons }: InferGetStaticPropsType<typeof getStaticProps>) {
+    console.log(lessons)
     return (
         <div className="flex flex-col gap-4 bg-gray-100 p-4 h-full">
             <Content id="1" current={true} time="40 cards" title="Introduction" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."/>
