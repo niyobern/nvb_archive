a3e2ca9937b914a84054f7a0ed7f176bf5dc48de
niyobern <niyobern@outlook.com>
2023-10-09 18:13:15 +0200

    updated question page

--- components/questtion.tsx
+++ components/questtion.tsx
@@ -1,14 +1,11 @@
-export default function Question() {
+export default function Question({ question }:any) {
     return (
         <div className="py-2 md:py-8 flex flex-col justify-center content-center">
             <div className="rounded bg-white shadow-lg shadow-gray-400 p-4 mx-auto flex flex-col">
                 <span className="text-2xl font-medium text-teal-900 text-center">Question 1</span>
-                <p className="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p className="text-justify">{question.question}</p>
                 <div className="flex flex-col gap-2 mt-2">
-                    <div className="rounded bg-gray-200 p-2 hover:bg-gray-300 hover:shadow-lg active:text-white cursor-pointer">Option 1 Lorem ipsum dolor</div>
-                    <div className="rounded bg-gray-200 p-2 hover:bg-gray-300 hover:shadow-lg active:text-white cursor-pointer">Option 1 Lorem ipsum dolor</div>
-                    <div className="rounded bg-gray-200 p-2 hover:bg-gray-300 hover:shadow-lg active:text-white cursor-pointer">Option 1 Lorem ipsum dolor</div>
-                    <div className="rounded bg-gray-200 p-2 hover:bg-gray-300 hover:shadow-lg active:text-white cursor-pointer">Option 1 Lorem ipsum dolor</div>
+                    {question.options && question.options.map((item: any) => <div className="rounded bg-gray-200 p-2 hover:bg-gray-300 hover:shadow-lg active:text-white cursor-pointer">Option 1 Lorem ipsum dolor</div>)}
                 </div>
             </div>
         </div>
--- pages/tests/index.tsx
+++ pages/tests/index.tsx
@@ -30,11 +30,16 @@ export const getServerSideProps = (async (context: any) => {
     return { props: { lessons: lessons } }
 })
 export default function Ibazwa({ lessons }: any){
-    const [questions, setQuestions] = useState()
+    const [questions, setQuestions] = useState([])
+    const [count, setCount] = useState(0)
     useEffect(() => {
         axios.get("https://nvb_backend-1-z3745144.deta.app/question")
-        .then((res: any) => console.log(res.data._items))
+        .then((res: any) => setQuestions(res.data._items))
     }, [])
+
+    function handleNavigate(move: number){
+        setCount(count + move)
+    }
     
     const start = Date.now()
     const duration = 20*60*1000
@@ -43,8 +48,8 @@ export default function Ibazwa({ lessons }: any){
             <div className="bg-teal-100 px-1 gap-4 md:px-10 py-2 md:py-4 flex flex-col h-full">
                 <Indicator start={start} duration={duration}/>
                 <Count/>
-                <Question/>
-                <Navigate test={true}/>
+                <Question question={questions[count]}/>
+                <Navigate test={true} move={handleNavigate}/>
             </div>
         </Layout>
     )
