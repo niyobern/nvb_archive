5bef0155e5aca5b51132777a1e4a32a34f35cab7
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-10-29 22:21:29 +0000

    Update questtion.tsx

--- components/questtion.tsx
+++ components/questtion.tsx
@@ -7,7 +7,7 @@ export default function Question({ question, count, answer, test }:any) {
                 <div className="rounded bg-white shadow-lg shadow-gray-400 p-4 flex flex-col">
                     <span className="text-2xl font-medium text-teal-900 text-center">Question {count + 1}</span>
                     <p className="text-justify">{question.question}</p>
-                { question.banner && <Image src={`../public/images/images/${question.banner}`} alt="Image" height={500} width={500} className="h-48 md:h-64 rounded-sm self-center w-full"/>}
+                { question.image && <Image src={`../public/images/images/${question.image}`} alt="Image" height={500} width={500} className="h-48 md:h-64 rounded-sm self-center w-full"/>}
                     <div className="flex flex-col gap-2 mt-2 h-fit">
                         {question.options && question.options.map((item: any, index: number) => <div onClick={() => answer(count, index + 1, 1)} key={index} className={`${item === "" ? "hidden" : "active:text-white"} bg-gray-200 rounded p-2 hover:bg-gray-300 hover:shadow-lg active:text-white cursor-pointer`}>{item}</div>)}
                         { question.images && question.images.map((item: any, index: any) => <Image key={index} src={`../public/images/images/${item}`} alt="Image" height={500} width={500} className="h-48 md:h-64 rounded-sm self-center w-full"/>)}
