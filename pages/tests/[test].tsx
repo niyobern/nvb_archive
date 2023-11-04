4e2e1fde1528c5701df3eb1c72c0c21767399c8a
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-11-04 23:05:34 +0000

    Update [test].tsx

--- pages/tests/[test].tsx
+++ pages/tests/[test].tsx
@@ -58,7 +58,7 @@ export default function Ibazwa({ links, questions, slug }: any){
         if (Number(time) + duration < Date.now()){
             setSubmit(true)
         }
-    }, [start, duration, submit])
+    })
     const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     const updatedLinks = {...links}
     updatedLinks.right = []
