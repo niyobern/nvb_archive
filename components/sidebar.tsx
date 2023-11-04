0578f8c76ef4ace979ffadd399387a813b8ce153
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-11-04 12:06:19 +0000

    Update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,7 +4,7 @@ export default function SideBar( { data, title }: any){
         <div className="w-full bg-teal-700 py-4  max-h-screen overflow-y-scroll scrollbar-thin">
             <ul className="pl-4 pr-2 flex flex-col gap-2 text-white font-normal h-fit">
                 <li className="text-lg font-bold">{title}</li>
-                {data.map((item: any) => <Link href={item.link} key={item.text}><li className="hover:text-lg hover:font-medium active:text-red-500 cursor-pointer">{item.text}</li></Link>)}
+                {data.map((item: any) => <Link href={item.link} key={item.text}><li className="h-fit hover:text-lg hover:font-medium active:text-red-500 cursor-pointer">{item.text}</li></Link>)}
             </ul>
         </div>
     )
