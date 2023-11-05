53904fa7ced2cac10bb0bc72ffa814406de81332
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-11-05 19:46:00 +0000

    Update logout.tsx

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 export default function Logout(){
-    conrouter = useRouter()
+    const router = useRouter()
     useEffect( ()=> {
         localStorage.removeItem("token")
         router.push("/")
