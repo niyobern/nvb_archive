6516323a2b96975e442a3d968d724b514b50d279
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-11-05 21:04:44 +0000

    Update questtion.tsx

--- components/questtion.tsx
+++ components/questtion.tsx
@@ -10,7 +10,7 @@ export default function Question({ question, count, answer, test }:any) {
                 { question.image && <Image priority={true} src={question.image} alt="Image" height={500} width={500} className="h-48 md:h-64 rounded-sm self-center w-fit"/>}
                     <div className="flex flex-col gap-2 mt-2 h-fit">
                         {question.answers && question.answers[0].slice(0,36) !== "https://d1yurxstr9hyp.cloudfront.net" && question.answers.map((item: any, index: number) => <div onClick={() => answer(count, index + 1, 1)} key={index} className={`${item === "" ? "hidden" : "active:text-white"} bg-gray-200 rounded p-2 hover:bg-gray-300 hover:shadow-lg active:text-white cursor-pointer`}>{item}</div>)}
-                        {question.answers && question.answers[0].slice(0,36) === "https://d1yurxstr9hyp.cloudfront.net" && question.answers.map((item: any, index: any) => <Image priority={true} onClick={() => answer(count, index + 1, 1)} key={index} src={item} alt="Image" height={500} width={500} className="h-48 md:h-64 rounded-sm self-center w-fit"/>)}
+                        {question.answers && question.answers[0].slice(0,36) === "https://d1yurxstr9hyp.cloudfront.net" && question.answers.map((item: any, index: number) => <Image priority={true} onClick={() => answer(count, index + 1, 1)} key={index} src={item} alt="Image" height={500} width={500} className="h-48 md:h-64 rounded-sm self-center w-fit"/>)}
                     </div>
                 </div>
             </div>
@@ -23,7 +23,7 @@ export default function Question({ question, count, answer, test }:any) {
                 <p className="text-justify">{question.question}</p>
                 { question.image && <Image src={question.image} alt="Image" height={500} width={500} className="h-48 md:h-64 rounded-sm self-center w-fit"/>}
                 <div className="flex flex-col gap-2 mt-2 h-fit">
-                    {question.answers && question.answers.map((item: any, index: number) => <div key={index} className={`${index + 1 == answer && index + 1 != question.answer? "bg-red-500": "bg-gray-200"} ${index + 1 == question.answer && index + 1 == answer? "bg-green-800 text-white" : " bg-gray-200"} ${index + 1 == question.answer && index + 1 != answer? "bg-green-400 text-white" : " bg-gray-200"} ${item === "" ? "hidden" : "active:text-white"} rounded p-2 hover:bg-gray-300 hover:shadow-lg active:text-white cursor-pointer`}>{item}</div>)}
+                    {question.answers && question.answers.map((item: any, index: number) => <div key={index} className={`${index + 1 == answer && index + 1 != question.answer? "bg-red-500": "bg-gray-200"} ${index + 1 == question.answer && index + 1 == answer? "bg-green-800 text-white" : " bg-gray-200"} ${index + 1 == question.answer && index + 1 != answer? "bg-green-400 text-white" : " bg-gray-200"} rounded p-2 hover:bg-gray-300 hover:shadow-lg active:text-white cursor-pointer`}>{item}</div>)}
                 </div>
             </div>
         </div>
